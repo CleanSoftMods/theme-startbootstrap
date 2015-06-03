d2267f717f0f485bf0ebfa25e77094960dfa4ab5
xLink <xLink@cybershade.org>
2015-06-03 14:03:59 +0100

    added support for sidebars

--- views/partials/theme/sidebar-left.blade.php
+++ views/partials/theme/sidebar-left.blade.php
@@ -1,1 +1,14 @@
-<p>sidebar-left</p>
+@if (count($menus))
+    @set($menuSet, Theme::getSidebar())
+
+    @if (isset($menus[$menuSet]) && !empty($menus[$menuSet]))
+        @foreach($menus[$menuSet] as $menu)
+
+            @set($render, array_get($menu, 'render_in', '_panel'))
+            @set($view, 'sidebar-items.'.$render)
+
+            {!! Theme::partial($view, compact('menu')) !!}
+
+        @endforeach
+    @endif
+@endif
