02a7018d44153ca2a9db8c60164a16cc17a0342a
Dan Aldridge <xLink@cybershade.org>
2015-06-12 14:48:04 +0100

    updated gulp, and re-compiled the assets

--- gulpfile.js
+++ gulpfile.js
@@ -3,37 +3,20 @@ var shell = require('gulp-shell');
 var elixir = require('laravel-elixir');
 var themeInfo = require('./theme.json');
 
-elixir.extend('themePublish', function() {
-    gulp.task('themePublish', ['less', 'scripts'], function() {
-        gulp.src('').pipe(shell('php ../../artisan theme:publish '+themeInfo.name));
-    });
-
-    this.registerWatcher('themePublish', '**/*.less');
-
-    return this.queueTask('themePublish');
-});
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir.config.publicDir = '../../public';
-elixir.config.cssOutput = 'assets/css';
-elixir.config.jsOutput = 'assets/js';
+// load the config class & boot the methods
+config = require('../../resources/assets/js/elixir-config');
+for (var method in config) {
+    if (typeof config[method] == "function") {
+        config[method](elixir, gulp, shell, themeInfo);
+    }
+}
 
 elixir(function(mix) {
     mix.less('app.less');
     mix.scripts([
         'assets/vendor/jquery/dist/jquery.js',
         'assets/vendor/bootstrap/dist/js/bootstrap.js',
-        'assets/js/application.js'
+        '../../../resources/assets/js/init.js'
     ], null, 'resources');
     mix.copy('resources/assets/vendor/font-awesome/fonts', 'assets/fonts');
     mix.copy('resources/assets/vendor/bootstrap/fonts', 'assets/fonts');
