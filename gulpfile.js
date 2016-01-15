93fda945dae3aefa2abe3426a9b0fd7ba699285d
xLink <xLink@cybershade.org>
2016-01-15 23:51:29 +0000

    made the gulp/elixir stuff work again

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,25 @@
 var gulp = require('gulp');
-var shell = require('gulp-shell');
 var elixir = require('laravel-elixir');
 var themeInfo = require('./theme.json');
 
 // load the config class & boot the methods
 config = require('../../resources/assets/js/elixir-config');
-for (var method in config) {
-    if (typeof config[method] == "function") {
-        config[method](elixir, gulp, shell, themeInfo);
-    }
-}
+config.setConfig(elixir);
 
 elixir(function(mix) {
-    mix.less('app.less');
+    // sort the scripts out
     mix.scripts([
         'assets/vendor/jquery/dist/jquery.js',
         'assets/vendor/bootstrap/dist/js/bootstrap.js',
         '../../../resources/assets/js/init.js'
     ], null, 'resources');
-    mix.copy('resources/assets/vendor/font-awesome/fonts', 'assets/fonts');
-    mix.copy('resources/assets/vendor/bootstrap/fonts', 'assets/fonts');
-    mix.themePublish();
+
+    // copy the fonts over
+    mix.copy('resources/assets/vendor/font-awesome/fonts', 'assets/fonts')
+    .copy('resources/assets/vendor/bootstrap/fonts', 'assets/fonts');
+
+    // do the style stuff, and send to browser everytime we change it
+    mix.less('app.less', 'assets/css/app.css')
+        .exec('php ../../artisan theme:publish ' + themeInfo.name, '**/*.less');
+
 });
