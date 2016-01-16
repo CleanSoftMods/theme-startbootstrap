5deae441786fa5b21c157ef79b4cf7f5f216f1a9
xLink <xLink@cybershade.org>
2016-01-16 00:19:13 +0000

    replaced login decoration with the one form the social module

--- views/partials/core/login.blade.php
+++ views/partials/core/login.blade.php
@@ -5,9 +5,12 @@
 
     <div class="panel panel-default">
         <div class="panel-heading">
-            <div class="panel-title">Login Form</div>
+            <div class="panel-title">Please Sign In
+                <a href="{{ route('pxcms.user.register') }}" class="btn btn-default btn-sm pull-right">Register</a>
+            </div>
         </div>
         <div class="panel-body">
+            <h4>Direct Login</h4>
             {!! Theme::partial('core._login_form') !!}
         </div>
     </div>
