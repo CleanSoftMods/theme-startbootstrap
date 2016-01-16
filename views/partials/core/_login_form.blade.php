5d8b55c345dc97441bc4abe6f22c1a3c20469342
xLink <xLink@cybershade.org>
2016-01-16 00:06:40 +0000

    fixed login button being misaligned

--- views/partials/core/_login_form.blade.php
+++ views/partials/core/_login_form.blade.php
@@ -4,7 +4,7 @@
     {!! Former::password('password', 'Password')->required() !!}
 
     <div class="form-group">
-        <div class="col-md-10 col-md-offset-2">
+        <div class="col-md-9 col-md-offset-3">
             <div class="checkbox">
                 <label><input type="checkbox" name="remember"> Remember Me </label>
             </div>
@@ -12,7 +12,7 @@
     </div>
 
     <div class="form-group">
-        <div class="col-md-10 col-md-offset-2">
+        <div class="col-md-9 col-md-offset-3">
             <button type="submit" class="btn btn-success">Login</button>
             <a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
         </div>
